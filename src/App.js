00ee6bda1ba15991bbfeb89dcdf3204e28089a6b
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-26 17:29:02 +0300

    Add new component - StopWatch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import StopWatch from './components/StopWatch';
 
 function App() {
   return (
     <>
+    <StopWatch/>
     </>
   );
 }
---
+++ src/components/StopWatch/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import s from "./StopWatch.module.css";
+
+class StopWatch extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <></>;
+  }
+}
+
+export default StopWatch;
