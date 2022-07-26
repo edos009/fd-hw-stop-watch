67bd92c1e9dda6836813c0c31738bb7e167687ce
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-26 17:50:39 +0300

    Add structure for component - StopWatch

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -4,10 +4,32 @@ import s from "./StopWatch.module.css";
 class StopWatch extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      time: new Date(0, 0, 0, 0, 0, 0),
+    };
   }
 
+  handlerStart = () => {};
+
+  handlerStop = () => {};
+
+  handlerRestart = () => {};
+
   render() {
-    return <></>;
+    const { time } = this.state;
+    return (
+      <>
+        <h1>Stopwatch</h1>
+        <article>
+          <p>{time.toLocaleTimeString("en-GB")}</p>
+          <div>
+            <button onClick={this.handlerStart}>Start</button>
+            <button onClick={this.handlerStop}>Stop</button>
+            <button onClick={this.handlerRestart}>Restart</button>
+          </div>
+        </article>
+      </>
+    );
   }
 }
 
